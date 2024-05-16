371b8ee681510240322c7fc1922c0e51ab5241f0
Manhvjpro <huynhmanh06272003@gmail.com>
2024-05-16 19:02:19 +0700

    first commit

---
+++ src/Ex2.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+// const App1 = () => {
+//   return (
+//     <div className="hello-react">
+//       Hello <span className="react-text">React</span>
+//     </div>
+//   );
+// };
+// export default App1;
+const App2 = () => {
+  return <div style={{ textAlign: "center", color: "blue" }}></div>;
+};
+export default App2;
